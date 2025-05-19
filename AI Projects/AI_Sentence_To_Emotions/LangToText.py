763d9aa678c13b16ca3809ebbfa2ea465be76666
5k3i <delongchampskyler@gmail.com>
2025-05-19 14:09:33 -0700

    Audio to Text workks

--- AI Projects/AI_Sentence_To_Emotions/LangToText.py
+++ AI Projects/AI_Sentence_To_Emotions/LangToText.py
@@ -1,6 +1,19 @@
 import speech_recognition as sr
-import pyaudio 
+import pyaudio
 
-r = sr.Recognizer()
 
-print("Running")
+def pull_mic_audio(speak_time) :
+    init_rec = sr.Recognizer()
+    print("Let's speak!!")
+    with sr.Microphone() as source:
+        audio_data = init_rec.record(source, duration=speak_time)
+        print("Recognizing your text.............")
+        text = init_rec.recognize_google(audio_data)
+        return text
+
+
+def main() :
+    print(pull_mic_audio(2))
+
+if __name__ == '__main__':
+    main()
